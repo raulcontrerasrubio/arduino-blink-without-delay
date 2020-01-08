afbbaf9cdeda19d0aaa64ce2ebdc0460a7253aac
Raúl Contreras Rubio <raulc98@gmail.com>
2020-01-08 20:18:36 +0100

    Converted ino file to cpp (#1)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
-const int LED = LED_BUILTIN;
+#include <Arduino.h>
+
+const int LED = 13;
 int pinState;
 
 // Beat {300->OFF, 200->ON};
@@ -6,6 +8,24 @@ int pattern[] = {300, 200, 300, 200, 300, 300, 150, 50, 150, 50, 150, 500, 200, 
 int PATTERN_SIZE = sizeof(pattern)/sizeof(pattern[1]);
 int currentPatternPosition;
 unsigned long storedTime;
+
+int changeCurrentPosition(int len, int currentPosition){
+  if(currentPosition == len - 1){
+    return 0;
+  }
+  return ++currentPosition;
+}
+
+int changePinState(int pinState){
+  if(pinState == HIGH){
+    return LOW;
+  }
+  return HIGH;
+}
+
+unsigned long getTimeSinceChange(unsigned long prevTime){
+  return millis() - prevTime;
+}
 
 void setup() {
   pinMode(LED, OUTPUT);
@@ -26,20 +46,14 @@ void loop() {
   digitalWrite(LED, pinState);
 }
 
-unsigned long getTimeSinceChange(unsigned long prevTime){
-  return millis() - prevTime;
-}
+int main(void){
+  init();
 
-int changePinState(int pinState){
-  if(pinState == HIGH){
-    return LOW;
+  setup();
+  for(;;){
+    loop();
+    if(serialEventRun) serialEventRun();
   }
-  return HIGH;
-}
 
-int changeCurrentPosition(int len, int currentPosition){
-  if(currentPosition == len - 1){
-    return 0;
-  }
-  return ++currentPosition;
+  return 0;
 }
